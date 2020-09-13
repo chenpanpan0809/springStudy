8c54f7d730dfb7290acf9366c4097d09324420f1
chenpanpan <13396073613@163.com>
2020-09-14 01:09:25 +0800

    Initial commit

---
+++ src/main/java/com/study/panpan/HelloController.java
@@ -0,0 +1,19 @@
+package com.study.panpan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/{name}")
+    public ModelAndView hello(@PathVariable String name){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name",name);
+        modelAndView.setViewName("hello");
+        return modelAndView;
+    }
+}
